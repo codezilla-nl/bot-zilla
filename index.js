6d55141126c3019db4cf64fb0e8dd6b33bca4e66
Jelle Kralt <jelle@jellekralt.nl>
2016-07-06 20:35:39 +0200

    Changed welcome text

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ if (token) {
 }
 
 controller.on('bot_channel_join', function (bot, message) {
-  bot.reply(message, "I'm here!")
+  bot.reply(message, "RRRAAAAHHHWWRR!")
 })
 
 controller.hears(['hello', 'hi'], ['direct_mention'], function (bot, message) {
@@ -52,24 +52,24 @@ controller.hears('help', ['direct_message', 'direct_mention'], function (bot, me
   bot.reply(message, help)
 })
 
-controller.hears(['attachment'], ['direct_message', 'direct_mention'], function (bot, message) {
-  var text = 'Beep Beep Boop is a ridiculously simple hosting platform for your Slackbots.'
-  var attachments = [{
-    fallback: text,
-    pretext: 'We bring bots to life. :sunglasses: :thumbsup:',
-    title: 'Host, deploy and share your bot in seconds.',
-    image_url: 'https://storage.googleapis.com/beepboophq/_assets/bot-1.22f6fb.png',
-    title_link: 'https://beepboophq.com/',
-    text: text,
-    color: '#7CD197'
-  }]
-
-  bot.reply(message, {
-    attachments: attachments
-  }, function (err, resp) {
-    console.log(err, resp)
-  })
-})
+// controller.hears(['attachment'], ['direct_message', 'direct_mention'], function (bot, message) {
+//   var text = 'Beep Beep Boop is a ridiculously simple hosting platform for your Slackbots.'
+//   var attachments = [{
+//     fallback: text,
+//     pretext: 'We bring bots to life. :sunglasses: :thumbsup:',
+//     title: 'Host, deploy and share your bot in seconds.',
+//     image_url: 'https://storage.googleapis.com/beepboophq/_assets/bot-1.22f6fb.png',
+//     title_link: 'https://beepboophq.com/',
+//     text: text,
+//     color: '#7CD197'
+//   }]
+//
+//   bot.reply(message, {
+//     attachments: attachments
+//   }, function (err, resp) {
+//     console.log(err, resp)
+//   })
+// })
 
 controller.hears('.*', ['direct_message', 'direct_mention'], function (bot, message) {
   bot.reply(message, 'Sorry <@' + message.user + '>, I don\'t understand. \n')
